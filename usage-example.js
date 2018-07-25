368d330653a0553f31d4943c37b89ff5ef5ea5f9
DavidWells <hello@davidwells.io>
2018-07-24 22:32:07 -0700

    refactor to allow for multiple caches

--- usage-example.js
+++ usage-example.js
@@ -2,12 +2,38 @@ const path = require('path')
 const cacheMagic = require('./lib')
 
 // const cacheDir = path.resolve('./opt/buildhome/cache')
-const cacheDir = path.resolve('./cache')
+const cacheFolder = path.resolve('./cache')
 
 // Do it
 const options = {
-  directory: path.join(__dirname),
-  cacheDir: cacheDir,
+  // directorys or files to cache
+  // contents: path.join(__dirname, 'two'),
+  contents: [
+    {
+      path: path.join(__dirname, 'two/node_modules'),
+      // TODO finish Cache Invalidator
+      invalidateOn: path.join(__dirname, 'two/package.json'),
+      command: 'npm install'
+    },
+    {
+      path: path.join(__dirname, 'other/node_modules'),
+      invalidateOn: path.join(__dirname, 'other/package.json'),
+      command: 'npm install'
+    },
+    {
+      path: path.join(__dirname, 'serverless-test/.serverless'),
+      invalidateOn: 'serverless.yml',
+      command: 'echo "hi"'
+    }
+  ],
+  // cache folder destination
+  cacheFolder: cacheFolder,
+
+  ignoreIfFolderExists: false,
+  // TODO finish Cache Invalidator
+  invalidateOn: () => {
+    return ''
+  }
 }
 
 cacheMagic(options).then(() => {
